0f06e4d9247c6a63552ae7620537c2368bd105e3
aashita822-byte <aashita822@gmail.com>
2026-05-12 22:47:18 +0530

    Update CADashboard.tsx

--- src/CADashboard.tsx
+++ src/CADashboard.tsx
@@ -374,7 +374,7 @@ const CADashboard: React.FC = () => {
       const payload = {
         pdf_s3_url:   item.pdf_url,
         dashboard_id: dashboardId,
-        platform: "cs",
+        platform: "ca",
         use_gemini:   true,
         use_openai:   true,
       };
